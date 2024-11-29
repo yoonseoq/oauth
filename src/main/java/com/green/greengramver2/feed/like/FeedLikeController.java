bc4fd37f30284a374b9768b3eb82314acd3c0bab
DoheumPark <pirbak@hanmail.net>
2024-11-29 15:42:08 +0900

    피드 like 완료

---
+++ src/main/java/com/green/greengramver2/feed/like/FeedLikeController.java
@@ -0,0 +1,29 @@
+package com.green.greengramver2.feed.like;
+
+import com.green.greengramver2.common.model.ResultResponse;
+import com.green.greengramver2.feed.like.model.FeedLikeReq;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("feed/like")
+public class FeedLikeController {
+    private final FeedLikeService service;
+
+    @GetMapping
+    public ResultResponse<Integer> feedLikeToggle(@ParameterObject @ModelAttribute FeedLikeReq p) {
+        log.info("FeedLikeController > feedLikeToggle > p: {}", p);
+        int result = service.feedLikeToggle(p);
+        return ResultResponse.<Integer>builder()
+                .resultMessage(result == 0 ? "좋아요 취소" : "좋아요 등록")
+                .resultData(result)
+                .build();
+    }
+}
