83941e3c2a45c591b1dea4fce6afdb81136df2ac
DoheumPark <pirbak@hanmail.net>
2024-12-19 12:28:38 +0900

    JWT 인증 처리 완료 (2) follow부분 추가 수정

--- src/main/java/com/green/greengram/user/follow/UserFollowService.java
+++ src/main/java/com/green/greengram/user/follow/UserFollowService.java
@@ -1,5 +1,6 @@
 package com.green.greengram.user.follow;
 
+import com.green.greengram.config.security.AuthenticationFacade;
 import com.green.greengram.user.follow.model.UserFollowReq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,12 +11,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserFollowService {
     private final UserFollowMapper mapper;
+    private final AuthenticationFacade authenticationFacade;
 
     public int postUserFollow(UserFollowReq p) {
+        p.setFromUserId(authenticationFacade.getSignedUserId());
         return mapper.insUserFollow(p);
     }
 
     public int deleteUserFollow(UserFollowReq p) {
+        p.setFromUserId(authenticationFacade.getSignedUserId());
         return mapper.delUserFollow(p);
     }
 }
--- src/main/java/com/green/greengram/user/follow/model/UserFollowReq.java
+++ src/main/java/com/green/greengram/user/follow/model/UserFollowReq.java
@@ -1,5 +1,6 @@
 package com.green.greengram.user.follow.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -10,16 +11,19 @@ import java.beans.ConstructorProperties;
 @Getter
 @ToString
 public class UserFollowReq {
-    @JsonProperty("from_user_id")
-    @Schema(name = "from_user_id", description = "팔로워 유저 ID", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    @JsonIgnore
     private long fromUserId;
+
     @JsonProperty("to_user_id")
     @Schema(name = "to_user_id", description = "팔로잉 유저 ID", example = "2", requiredMode = Schema.RequiredMode.REQUIRED)
     private long toUserId;
 
-    @ConstructorProperties({"from_user_id", "to_user_id"})
-    public UserFollowReq(long fromUserId, long toUserId) {
-        this.fromUserId = fromUserId;
+    @ConstructorProperties({"to_user_id"})
+    public UserFollowReq(long toUserId) {
         this.toUserId = toUserId;
+    }
+
+    public void setFromUserId(long fromUserId) {
+        this.fromUserId = fromUserId;
     }
 }
